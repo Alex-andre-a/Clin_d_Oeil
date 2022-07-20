178b1f5657d9fce9719036c18a558a6940e8e2f3
Alex-andre-a <ptitmitch44@gmail.com>
2022-07-20 11:54:12 +0200

    Navbar en Js en cours !

--- script.js
+++ script.js
@@ -1,6 +1,20 @@
 const hamburger = document.querySelector(".hamBurger");
 const navLinks = document.querySelector(".navLinks");
+const closeNav = document.querySelector(".closeNav");
 
 hamburger.addEventListener("click", () => {
   navLinks.classList.toggle("active");
 });
+
+hamburger.addEventListener("click", () => {
+  hamburger.style.display = "none";
+});
+
+closeNav.addEventListener("click", () => {
+  navLinks.classList.remove("active");
+});
+
+closeNav.addEventListener("click", () => {
+  hamburger.style.display = "block";
+});
+
